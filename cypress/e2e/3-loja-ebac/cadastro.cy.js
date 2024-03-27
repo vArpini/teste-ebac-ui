18ee4816acac82336841332b7a6d879f861315c4
Vini <vini.arpini10@gmail.com>
2024-03-26 21:40:52 -0400

    Faker continuação

--- cypress/e2e/3-loja-ebac/cadastro.cy.js
+++ cypress/e2e/3-loja-ebac/cadastro.cy.js
@@ -14,15 +14,30 @@ describe('Funcionalidade: Cadastro', () => {
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(faker.person.firstName())
+        cy.get('#account_last_name').type(faker.person.lastName())
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+
     });
 
     it.only('Deve completar o cadastro com sucesso - Usando variáveis', () => {
         var email = faker.internet.email()
+        var nome = faker.person.firstName()
+        var sobrenome = faker.person.lastName()
 
         cy.get('#reg_email').type(email)
         cy.get('#reg_password').type('teste@123')
         cy.get(':nth-child(4) > .button').click()
 
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nome)
+        cy.get('#account_last_name').type(sobrenome)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
     });
 });
