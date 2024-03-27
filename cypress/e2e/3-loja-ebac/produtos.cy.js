34521247274234ada8b799d93ac72ade68fddf1b
Vini <vini.arpini10@gmail.com>
2024-03-26 21:48:46 -0400

    Continuação listas

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -9,6 +9,8 @@ describe('Funcionalidade: Produtos', () => {
     it('Deve selecionar um produto da lista', () => {
         cy.get('.product-block').first().click()
         // .last() ele pega o ultimo da lista
-        // .eq(2) ele pega o segundo da lista
+        // .eq(2) ele pega a posição 2 da lista
+        // .contains('Frankie Sweatshirt') ele pega o nome
+        cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
 });
