37a8b26418c52d89dda7abd2663a9713b3f80aff
Vini <vini.arpini10@gmail.com>
2024-04-14 16:46:27 -0400

    Massa de dados em lista

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -13,16 +13,28 @@ describe('Funcionalidade: Produtos', () => {
             cy.get('#tab-title-description > a').should('contain', 'Descricao')
     });
 
-    it.only('Deve buscar um produto com sucesso', () => {
+    it('Deve buscar um produto com sucesso', () => {
         produtosPage.buscarProduto('Zeppelin Yoga Pant')
         cy.get('.product_title').should('contain', 'Zeppelin Yoga Pant')
     });
 
     it('Deve visitar a página do produto', () => {
-        
+        produtosPage.visitarProduto('Zeppelin-Yoga-Pant')
+        cy.get('.product_title').should('contain', 'Zeppelin Yoga Pant')
     });
 
     it('Deve adicionar produto ao carrinho', () => {
+        produtosPage.buscarProduto('Abominable Hoodie')
+        produtosPage.addProdutoCarrinho('M', 'Green', 5)
+        cy.get('.woocommerce-message').should('contain', '5 × “Abominable Hoodie” foram adicionados no seu carrinho.')
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+            produtosPage.buscarProduto(dados[1].nomeProduto)
+            produtosPage.addProdutoCarrinho(dados[1].tamanho, dados[1].cor, dados[1].quantidade)
+            cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+        })
         
     });
 });
